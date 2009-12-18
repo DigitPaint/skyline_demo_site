c2ed71d9b02777e7b7d6bcce1d1eca3f37383672
Flurin Egger <flurin@digitpaint.nl>
2009-12-18 15:05:18 +0100

    Updated Schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(:version => 20091202152514) do
     t.text     "teaser"
     t.string   "title"
     t.string   "url_part"
-    t.text     "body",             :limit => 16777215
+    t.text     "body"
     t.integer  "image_id"
     t.integer  "integer"
     t.date     "publication_date"
-    t.boolean  "published",                            :default => false, :null => false
+    t.boolean  "published",        :default => false, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(:version => 20091202152514) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "locked",                        :default => false, :null => false
-    t.string   "url_part"
+    t.string   "url_part",                      :default => ""
     t.boolean  "persistent",                    :default => false, :null => false
     t.string   "identifier"
     t.string   "type"
@@ -79,136 +79,7 @@ ActiveRecord::Schema.define(:version => 20091202152514) do
   create_table "skyline_associated_tags", :force => true do |t|
     t.integer "taggable_id"
     t.integer "tag_id"
-    t.string  "taggable_type", :null => false
-  end
-
-  create_table "skyline_form_fields", :force => true do |t|
-    t.integer  "page_id"
-    t.string   "type",                           :null => false
-    t.integer  "field_id"
-    t.string   "identifier"
-    t.string   "template"
-    t.integer  "position",        :default => 0, :null => false
-    t.string   "title",                          :null => false
-    t.string   "prefix"
-    t.string   "suffix"
-    t.text     "body"
-    t.boolean  "selected"
-    t.string   "default_value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "multiple_select"
-    t.boolean  "required"
-  end
-
-  add_index "skyline_form_fields", ["field_id", "position"], :name => "index_skyline_form_fields_on_field_id_and_position"
-  add_index "skyline_form_fields", ["page_id", "position"], :name => "index_skyline_form_fields_on_page_id_and_position"
-
-  create_table "skyline_form_forms", :force => true do |t|
-    t.string   "title",                     :null => false
-    t.integer  "position",   :default => 0, :null => false
-    t.string   "template",                  :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "skyline_form_forms", ["position"], :name => "index_skyline_form_forms_on_position"
-
-  create_table "skyline_form_handlers", :force => true do |t|
-    t.string   "handleable_type",                :null => false
-    t.integer  "handleable_id",                  :null => false
-    t.string   "handlee_type",                   :null => false
-    t.integer  "handlee_id",                     :null => false
-    t.integer  "position",        :default => 0, :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "template"
-  end
-
-  add_index "skyline_form_handlers", ["handleable_type", "handleable_id", "position"], :name => "skyline_form_handlers_ht_hi_p"
-
-  create_table "skyline_form_handlers_email_handlers", :force => true do |t|
-    t.string   "to_email",         :null => false
-    t.string   "to_name"
-    t.string   "from_email",       :null => false
-    t.string   "from_name"
-    t.string   "subject"
-    t.text     "header"
-    t.text     "footer"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "email_handler_id"
-    t.integer  "form_section_id"
-  end
-
-  create_table "skyline_form_handlers_redirect_handlers", :force => true do |t|
-    t.integer  "linked_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "custom_url"
-    t.integer  "redirect_handler_id"
-    t.integer  "form_section_id"
-  end
-
-  create_table "skyline_form_handlers_storage_handlers", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "skyline_form_pages", :force => true do |t|
-    t.integer  "form_id",                   :null => false
-    t.string   "title"
-    t.integer  "position",   :default => 0, :null => false
-    t.string   "template"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "skyline_form_pages", ["form_id", "position"], :name => "index_skyline_form_pages_on_form_id_and_position"
-
-  create_table "skyline_form_response_values", :force => true do |t|
-    t.integer  "response_id",    :null => false
-    t.integer  "field_id",       :null => false
-    t.string   "string_value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "integer_value"
-    t.boolean  "boolean_value"
-    t.date     "date_value"
-    t.time     "time_value"
-    t.text     "text_value"
-    t.datetime "datetime_value"
-  end
-
-  add_index "skyline_form_response_values", ["response_id", "field_id"], :name => "index_skyline_form_response_values_on_response_id_and_field_id"
-
-  create_table "skyline_form_responses", :force => true do |t|
-    t.integer  "form_id",                            :null => false
-    t.string   "token",                              :null => false
-    t.boolean  "finished",        :default => false, :null => false
-    t.integer  "current_page_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "skyline_form_responses", ["form_id", "token"], :name => "index_skyline_form_responses_on_form_id_and_token"
-
-  create_table "skyline_form_validators", :force => true do |t|
-    t.string   "validatee_type",                  :null => false
-    t.integer  "validatee_id",                    :null => false
-    t.string   "validatable_type",                :null => false
-    t.integer  "validatable_id",                  :null => false
-    t.integer  "position",         :default => 0, :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "skyline_form_validators", ["validatee_type", "validatee_id", "position"], :name => "skyline_form_validators_vt_vi_p"
-
-  create_table "skyline_form_validators_required_validators", :force => true do |t|
-    t.string   "error_message"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string  "taggable_type", :default => "", :null => false
   end
 
   create_table "skyline_grants", :force => true do |t|
@@ -325,12 +196,6 @@ ActiveRecord::Schema.define(:version => 20091202152514) do
     t.datetime "updated_at"
   end
 
-  create_table "skyline_sections_form_sections", :force => true do |t|
-    t.integer  "form_id",    :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "skyline_sections_heading_sections", :force => true do |t|
     t.string   "heading",    :null => false
     t.datetime "created_at"
@@ -389,14 +254,14 @@ ActiveRecord::Schema.define(:version => 20091202152514) do
   end
 
   create_table "skyline_sections_wysiwyg_sections", :force => true do |t|
-    t.text     "body",       :limit => 16777215
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "skyline_tags", :force => true do |t|
     t.string "tag"
-    t.string "taggable_type", :null => false
+    t.string "taggable_type", :default => "", :null => false
   end
 
   add_index "skyline_tags", ["taggable_type", "tag"], :name => "index_skyline_tags_on_taggable_type_and_tag"
