5e76fd203b3c03ad028c6006d83f4c9559b4d4d7
Luis Doubrava <luis@digitpaint.nl>
2010-04-13 11:30:10 +0200

    added routes for order

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect 'search_suggestions', :controller => "search_suggestions", :action => "index"
   
+  map.resource :order
+  map.connect 'orders/:action', :controller => "orders"
+  
   map.rss 'rss/:model.:format', :controller => "rss", :action => "show"
   map.form_response '*url/response/:action', :controller => "skyline/site/responses"
   
